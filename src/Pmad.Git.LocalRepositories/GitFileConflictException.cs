582884f50abe08140ce45b81c7e417e0bafa4971
Julien <julien.etelain@outlook.com>
2026-01-26 00:05:21 +0100

    Comments

--- src/Pmad.Git.LocalRepositories/GitFileConflictException.cs
+++ src/Pmad.Git.LocalRepositories/GitFileConflictException.cs
@@ -1,13 +1,27 @@
-﻿namespace Pmad.Git.LocalRepositories
-{
-    public class GitFileConflictException : InvalidOperationException
-    {
-        public GitFileConflictException(string message, string filePath)
-            : base(message)
-        {
-            FilePath = filePath;
-        }
+﻿namespace Pmad.Git.LocalRepositories;
 
-        public string FilePath { get; init; }
+/// <summary>
+/// The exception that is thrown when a file conflict occurs during a Git operation.
+/// </summary>
+/// <remarks>This exception indicates that a file could not be processed due to a conflict, such as when merging
+/// or rebasing branches in a Git repository. The file path associated with the conflict is available in the <see
+/// cref="FilePath"/> property.</remarks>
+public sealed class GitFileConflictException : InvalidOperationException
+{
+    /// <summary>
+    /// Initializes a new instance of the GitFileConflictException class with a specified error message and the path of
+    /// the file that caused the conflict.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="filePath">The path to the file that is involved in the conflict.</param>
+    public GitFileConflictException(string message, string filePath)
+        : base(message)
+    {
+        FilePath = filePath;
     }
+
+    /// <summary>
+    /// Gets the path to the file that is involved in the conflict.
+    /// </summary>
+    public string FilePath { get; init; }
 }
